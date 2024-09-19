462149a91cbac3db45ac8a921c048cb29d0c95dd
Vinayak362005 <kusumsharma1234vinayak@gmail.com>
2024-09-19 18:45:27 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,21 @@
+"use strict";//treat all JS code as newer version
+//alert(3+3)  // we are using node.js, not browser
+console.log(3+3);
+console.log("Vinayk Sharma");
+let name = "Vinayak Sharma"
+let age = 18
+let isLoggedIn = false
+let state;
+
+// permitive data type:---
+// number=>2 to power 53
+// bigint
+// string =>""
+// boolean => true/false
+// null =>standalone value
+// symbol => unique
+
+// object
+console.log(typeof undefined"); //user defined
+console.log(typeof null); //object
+
