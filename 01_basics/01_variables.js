c27beaaec9b4e7847aad63e8f7440dd73cc81033
Vinayak362005 <kusumsharma1234vinayak@gmail.com>
2024-09-19 18:45:51 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 144553
+let accountEnail = "Vinayak Sharma@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur" 
+let accountState;
+//accountId = 2//Not Allowed
+accountEmail="hc@hc"
+accountPassword="21212121"
+accountCity="Himachal Pradesh"
+console.log(accountId);
+/*
+prefer not to use var
+because of issue in block scope and functional scope
+*/
+console.table([accountId,accountPassword,accountEmail,accountCity,accountState])
